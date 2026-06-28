4fd0a5c5e23debcfe47b9c019c2a364c64adce8f
Your Name <you@example.com>
2026-06-28 22:54:59 +0530

    addingfolders

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+import Express from "express";
+import dotenv from "dotenv";
+
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+
