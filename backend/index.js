8bc8475edae06784b6d835b7b6377a76c7caba68
Your Name <you@example.com>
2026-06-28 23:34:46 +0530

    setup

--- backend/index.js
+++ backend/index.js
@@ -4,20 +4,45 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT;
 
-app.get('/world', (req, res) => {
-  res.send('Hello World!');
-});
+
 
 app.get('/', (req, res) => {
   res.send('Welcome to our server!');
 });
 
+app.get('/jokes',(req,res)=>{
+    const jokes=[
+        {
+            id:1,
+            title:'first Joke',
+            content:'This is first joke'
+        },
+        {
+            id:2,
+            title:'second joke',
+            content:'this is second joke'
+        },
+        {
+            id:3,
+            title:'third joke',
+            content:'this is third joke'
+        },
+        {
+            id:4,
+            title:'fourth joke',
+            content:'this is fourth joke'
+        },
+        {
+            id:5,
+            title:'fifth joke',
+            content:'this is fifth joke'
+        }
+    ]
+    res.send(jokes);
+})
 
-app.get('/home', (req, res) => {
-  res.send('Welcome to our HomePage!');
-});
 
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`app listening on port ${port}`);
 });
