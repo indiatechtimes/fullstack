b554eb7f32869eb488993c402fc405e61bfdb946
Your Name <you@example.com>
2026-06-28 23:10:02 +0530

    setup

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,23 @@
-import Express from "express";
-import dotenv from "dotenv";
 
-
+require('dotenv').config();
+const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
-app.get('/', (req, res) => {
+app.get('/world', (req, res) => {
   res.send('Hello World!');
 });
+
+app.get('/', (req, res) => {
+  res.send('Welcome to our server!');
+});
+
+
+app.get('/home', (req, res) => {
+  res.send('Welcome to our HomePage!');
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-
-
